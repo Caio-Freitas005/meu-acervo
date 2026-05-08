877c5abf80a506fb2ca1c2ce4ed49dc6ccc2fd5e
Caio Freitas <caio078freitas@gmail.com>
2026-05-07 21:09:20 -0300

    Altera configuração do banco
    - Adiciona PRAGMA foreign_key = ON para integridade referencial das chaves estrangeiras
    - Adiciona comentários DROP TABLE para testes no expo go
    - Remove o campo favoritado temporariamente

--- src/database/db.js
+++ src/database/db.js
@@ -1,18 +1,26 @@
-import * as SQLite from "expo-sqlite"
+import * as SQLite from "expo-sqlite";
 
-let db = null
+let db = null;
 
 export const getDB = async () => {
-    if (!db) {
-        db = await SQLite.openDatabaseAsync('acervo.db')
-        await initSchema(db)
-    }
-    return db
-}
+  if (!db) {
+    db = await SQLite.openDatabaseAsync("acervo.db");
+    await initSchema(db);
+  }
+  return db;
+};
 
-const initSchema = async (db) => {
-    await db.execAsync(
-        `CREATE TABLE IF NOT EXISTS livros (
+export const initSchema = async (db) => {
+  // Necessário para o SQLite fazer o ligamento das foreign keys de forma correta
+  await db.execAsync("PRAGMA foreign_keys = ON;");
+
+  // Em caso de erro nos testes com o expo go relacionado a alteração da tabela
+  // descomente temporariamente
+  // await db.execAsync("DROP TABLE IF EXISTS anotacoes;");
+  // await db.execAsync("DROP TABLE IF EXISTS livros;");
+
+  await db.execAsync(
+    `CREATE TABLE IF NOT EXISTS livros (
             id_livro INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo TEXT,
             autor TEXT,
@@ -21,18 +29,17 @@ const initSchema = async (db) => {
             avaliacao INTEGER CHECK(avaliacao BETWEEN 0 AND 5),
             opiniao TEXT,
             status TEXT CHECK(status IN ('lido', 'lendo')),
-            ultima_pagina_lida INTEGER,
-            favoritado BOOLEAN
-        )`
-    )
+            ultima_pagina_lida INTEGER
+        )`,
+  );
 
-    await db.execAsync(
-        `CREATE TABLE IF NOT EXISTS anotacoes (
+  await db.execAsync(
+    `CREATE TABLE IF NOT EXISTS anotacoes (
             id_anotacao INTEGER PRIMARY KEY AUTOINCREMENT,
             titulo_anotacao TEXT,
             texto_anotacao TEXT,
             id_livro INTEGER,
             FOREIGN KEY (id_livro) REFERENCES livros(id_livro)
-        )`
-    )
-}
+        )`,
+  );
+};
