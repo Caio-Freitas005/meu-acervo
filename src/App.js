7a933700d8affee968533e4396a3f26768af150d
Caio Freitas <caio078freitas@gmail.com>
2026-05-07 21:04:05 -0300

    Adiciona inicialização do banco e tela de acervo App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { testeLivro } from './services/TestesDeBanco';
+import { getDB } from './database/db';
 import Home from './screens/Home/index'
-import TestesNav from './test/Screens/TestesNav';
+import Acervo from './screens/Acervo/index'
+import TestesNav from './tests/screens/TestesNav/index'
 
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+  const [bancoPronto, setBancoPronto] = useState(false);
+
+  useEffect(() => {
+    async function prepararBanco() {
+      try {
+        await getDB(); 
+        await testeLivro(); 
+        
+        setBancoPronto(true);
+      } catch (error) {
+        console.error("Erro fatal ao iniciar:", error);
+      }
+    }
+
+    prepararBanco();
+  }, []);
+
+  if (!bancoPronto) {
+    return (
+      <View style={styles.container}>
+        <Text>Preparando seu acervo...</Text>
+      </View>
+    );
+  }
+
   return (
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Acervo" component={Acervo} />
         <Tab.Screen name="Testes" component={TestesNav} />
       </Tab.Navigator>      
     </NavigationContainer>
-      
   );
 }
 
