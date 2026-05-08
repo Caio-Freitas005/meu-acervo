42e8911373b4946cb74fad5a559e0ade9674657a
Caio Freitas <caio078freitas@gmail.com>
2026-05-07 21:14:58 -0300

    Adiciona protótipo da tela de Acervo

---
+++ src/screens/Acervo/index.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import { View, Text, ActivityIndicator, FlatList} from "react-native";
+import { styles } from "./styles";
+import { getLivros } from "../../database/repositories/livroRepository";
+
+export default function Acervo({ navigation }) {
+  const [livros, setLivros] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  async function carregar() {
+    setLoading(true);
+    try {
+      const data = await getLivros();
+      setLivros(data || []);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    const unsubscribe = navigation.addListener("focus", carregar);
+    return unsubscribe;
+  }, [navigation]);
+
+
+  return (
+    <View style={styles.container}>
+      {loading ? (
+        <ActivityIndicator size="large" color="#4F46E5" style={styles.loader} />
+      ) : livros.length === 0 ? (
+        <Text style={styles.emptyText}>Nenhum livro cadastrado.</Text>
+      ) : (
+        <View>
+          <Text style={styles.title}>Meu Acervo 📚</Text>
+          <Text style={styles.subtitle}>
+            Bem-vindo à sua coleção de livros!
+          </Text>
+
+          <FlatList
+            data={livros}
+            keyExtractor={(item) => String(item.id_livro || Math.random())}
+            renderItem={({ item }) => (
+              <View style={styles.card}>
+                <View>
+                  <Text>{item.titulo}</Text>
+                  <Text>{item.autor}</Text>
+                  <Text>{item.ano_publicacao}</Text>
+                </View>
+              </View>
+            )}
+          />
+        </View>
+      )}
+    </View>
+  );
+}
