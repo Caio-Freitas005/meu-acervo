97c3b57b92bc90ff7313925d4116c246c188657d
Fatec <fatec@FPG129.LOCAL>
2026-05-18 20:11:22 -0300

    Protótipo tela gerenciarAnotação

---
+++ src/screens/GerenciarAnotacao/index.js
@@ -0,0 +1,73 @@
+import { useState, useEffect } from "react";
+import { Text, TextInput, TouchableOpacity, ScrollView } from "react-native";
+import { styles } from "./styles";
+import {
+    addAnotacao,
+    updateAnotacao,
+} from "../../database/repositories/anotacaoRepository";
+
+export default function GerenciarAnotacao({ navigation, route }) {
+    const anotacaoParaEditar = route.params?.anotacao;
+
+    const [titulo_anotacao, setTitulo] = useState("");
+    const [texto_anotacao, setTexto] = useState("");
+
+    useEffect(() => {
+        if (anotacaoParaEditar) {
+            setTitulo(anotacaoParaEditar.titulo_anotacao);
+            setTexto(anotacaoParaEditar.texto_anotacao);
+        }
+    }, [anotacaoParaEditar]);
+
+    async function salvar() {
+        if (!titulo_anotacao || !texto_anotacao) {
+            alert("Por favor, preencha os campos obrigatórios.");
+            return;
+        }
+
+        try {
+            if (anotacaoParaEditar) {
+                await updateAnotacao(
+                    anotacaoParaEditar.id_anotacao,
+                    titulo_anotacao,
+                    texto_anotacao,
+                );
+
+            } else {
+                await addAnotacao(
+                    titulo_anotacao,
+                    texto_anotacao
+
+                );
+            }
+            navigation.goBack();
+        } catch (error) {
+            console.error("Erro ao salvar a Anotação:", error);
+        }
+    }
+    return (
+        <ScrollView contentContainerStyle={styles.container}>
+            <Text style={styles.title}>
+                {anotacaoParaEditar ? "Editar Anotação" : "Novo Anotação"}
+            </Text>
+
+            <TextInput
+                placeholder="Título"
+                value={titulo_anotacao}
+                onChangeText={setTitulo}
+                style={styles.input}
+            />
+            <TextInput
+                placeholder="Texto"
+                value={texto_anotacao}
+                onChangeText={setTexto}
+                style={styles.input}
+            />
+
+
+            <TouchableOpacity onPress={salvar} style={styles.button}>
+                <Text style={styles.buttonText}>Salvar Anotação</Text>
+            </TouchableOpacity>
+        </ScrollView>
+    );
+}
---
+++ src/screens/GerenciarAnotacao/styles.js
@@ -0,0 +1,56 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#F5F5F5",
+    padding: 24,
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: "#333",
+    textAlign: "center",
+    marginBottom: 8,
+  },
+  subtitle: {
+    fontSize: 16,
+    color: "#666",
+    textAlign: "center",
+    marginBottom: 40,
+  },
+  card: {
+    backgroundColor: "#FFFFFF",
+    padding: 20,
+    borderRadius: 12,
+    marginBottom: 24,
+    elevation: 2,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+  },
+  cardTitle: {
+    fontSize: 18,
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 12,
+  },
+  cardText: {
+    fontSize: 16,
+    color: "#555",
+    marginBottom: 8,
+  },
+  button: {
+    backgroundColor: "#6200ee",
+    paddingVertical: 16,
+    borderRadius: 10,
+    alignItems: "center",
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
